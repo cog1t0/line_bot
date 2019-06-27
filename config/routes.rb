ab8ef1d3da59481edb8b31c5ce3fa91e4128e218
cog1t0 <riku4724@gmail.com>
2019-06-27 23:26:23 +0900

    for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   get '/user/hello', to: 'users#hello'
   get '/user/test', to: 'users#test'
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
