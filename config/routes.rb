2d02fb165198135217c71eace09ad86508c2243b
cog1t0 <riku4724@gmail.com>
2019-06-28 00:35:23 +0900

    id

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/user/hello', to: 'users#hello'
   get '/user/test', to: 'users#test'
   root 'users#index'
+
+  post '/webhook', to: 'webhook#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
